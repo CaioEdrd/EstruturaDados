29e3abb23c69203363a690a0750ce8652f56a7d3
CaioEduardo <caio230703@gmail.com>
2025-10-09 17:02:18 -0300

    tratamento de erros

--- quickSort.py
+++ quickSort.py
@@ -1,20 +1,19 @@
 def quickSort(lista):
         
-        if len(lista) == 1 :
+        if len(lista) <= 1 :
             return lista  
-        elif len(lista) > 0:
+        else :
             central = len(lista)//2 
             pivo = lista[central]
-        else:
-            return []
+        
         
         esquerda = [x for x in lista if x < pivo]
-        meio =  [pivo]
+        meio =  [x for x in lista if x == pivo]
         direita = [x for x in lista if x > pivo]
 
         return quickSort(esquerda) + meio + quickSort(direita)
 
-listaDesordenada = [1,5,7,9,2,15,4] #1,2,4,5,7,9,15 -> Pivo 5
+listaDesordenada = [1,5,7,9,2,15,4,18,22,33,6] # -> Pivo 15 -> 1,2,4,5,6,7,9,15,18,22,33 
 print(quickSort(listaDesordenada))
 
 listaDesordenada2 = [1,7,6,5,8,3,4,122,15,14,199]
