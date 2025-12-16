d47baae810e71324be2443269af3cee9e0b18a5c
Caio Eduardo <caio230703@gmail.com>
2025-12-16 11:49:15 -0300

    adicção de novos nós

--- ChegueAoTopoV2.py
+++ ChegueAoTopoV2.py
@@ -124,8 +124,13 @@ Há uma fogueira apagada e um mapa rasgado.
 Você vê um ancião estranho, pode ignorar ele e explorar o acampamento antigo
 """)
 ponte_arriscada = N('Ponte_arriscada', """
-A ponte range quando você pisa nela.
-Você pode voltar e ir para a outra ponte ou arriscar pular sobre essa ponte que já está...
+A ponte é formada por tábuas velhas presas por cordas gastas. O abismo abaixo é profundo o suficiente para não se ver o fundo. A cada passo, a estrutura range e balança, deixando claro que pode ceder a qualquer momento
+""")
+atravessar_com_cuidado = N("Atravessar_Com_Cuidado","""
+Segurando firmemente nas cordas laterais, o avanço é lento e tenso. Algumas tábuas cedem sob o peso, mas ainda se mantêm presas. O vento balança a ponte, exigindo concentração total para não perder o equilíbrio.
+""")
+descer_pelo_abismo = N("Descer_pelo_Abismo", """
+Um caminho estreito segue pela lateral do penhasco. A descida é íngreme e escorregadia, mas parece mais estável do que a ponte. Qualquer erro pode resultar em uma queda grave.
 """)
 neblina = N('Neblina', """
 Uma neblina espessa desce rápido.
@@ -161,9 +166,7 @@ Você encontra um barracão em ruínas com símbolos antigos.
 Você pode  tentar decifrar esses símbolos que parece dar em um tesouro escondido alí, seguir uma trilha para tentar chegar ao topo e conseguir ajuda ou ir até o moinho do acampamento
 """)
 moinho = N('Moinho', """
-Um velho moinho de vento aparece no topo de uma colina baixa.
-Há uma velha corda pendurada e nada mais.
-Seguindo em frente você tem uma trilha, você pode arriscar ir por ela ou tentar subir na corda para ver o que acha.
+As pás quebradas do moinho giram lentamente, mesmo sem vento aparente. A madeira estala, e o som grave do mecanismo ecoa ao redor. O lugar parece fora do tempo, como se ainda tentasse cumprir sua função, apesar de abandonado há anos.
 """)
 bosque_claro = N('Bosque_claro', """
 O bosque se abre em uma clareira com flores estranhas.
@@ -204,14 +207,17 @@ Você pode estudar o mapa mentalmente.
 Logo abaixo há uma neblina forte e logo ao seu lado tem marcas de pegadas que estão sumindo
 """)
 passo_raso = N('Passo_raso', """
-Um passo raso exige equilíbrio.
-Você conseguiu passar com calma, mas chega em uma falésia você terá que arriscar tudo e pular para tentar chegar ao outro lado...
+A água parece calma à primeira vista, cobrindo apenas parte dos tornozelos. No entanto, a corrente empurra com força, e as pedras sob a superfície estão cobertas por limo escorregadio. O barulho constante da água abafa outros sons, tornando difícil perceber qualquer perigo se aproximando.
 """)
 refugio = N('Refugio', """
-Um pequeno refúgio de pedra oferece abrigo.
-Há mantimentos secos.
-Você pode seguir para o acampamento que está próximo ou ir até o mirante que você está vendo da posição atual
+Restos de uma fogueira ainda soltam fumaça fina, indicando que alguém esteve ali recentemente. Latas vazias, pedaços de tecido e marcas no chão sugerem pressa na partida. O local oferece abrigo, mas também levanta dúvidas sobre quem passou por ali.
 """)
+explorar_acampamento = N('Explorar_Acampamento', """
+Entre mochilas rasgadas e caixas reviradas, há mapas antigos e anotações incompletas. Algumas rotas estão marcadas com pressa, como se alguém tivesse descoberto algo importante e precisasse sair rápido.
+""")
+trilha_dos_sinais = N("Trilha_dos_Sinais","""
+Pedras empilhadas, galhos quebrados e marcas no chão indicam um caminho improvisado. Alguém claramente queria deixar um rastro, mas não se sabe se era para ajudar… ou atrair.
+""""")
 encosta_rochosa = N('Encosta_rochosa', """
 A encosta é rochosa e exige técnica.
 Pode seguir em passos rasos tomando cuidado ou tentar subir...
@@ -247,7 +253,21 @@ trilha_antiga = N('Trilha_antiga',
 tempestade = N('tempestade',
                'O céu escurece rapidamente. Tempestade se aproxima. Você pode correr até o refúgio que passou anteriormente ou corre em frente por uma ponte de madeira que parece')
 som_metalico = N('Som_metalico',
-                  'Um som metálico ecoa por entre as rochas. Ao seguir o caminho do som, você vê a uma certa distância algumas fogueiras apagadas que parece ser ruinas de algo, mas ao seu lado há um bosque bastante chamativo')
+                  'Um ruído metálico ecoa entre as pedras, irregular e insistente, como ferro sendo arrastado contra rocha. O som se propaga pelo vale e se mistura ao vento, dificultando saber de onde vem exatamente. Às vezes parece distante, outras vezes próximo demais, fazendo a sensação de estar sendo observado crescer a cada passo.')
+estrutura_abandonada = N("Estrutura_ambandonada",
+                         "Entre a vegetação densa e pedras cobertas de musgo, surge uma antiga estrutura de metal, tomada pela ferrugem. Vigas tortas e correntes soltas balançam lentamente, produzindo o som que ecoa pela região. Tudo indica que aquilo foi construído há muito tempo, mas abandonado às pressas.")
+sala_das_engrenagens = N('Sala_das_Engrenagens',
+                         "O interior revela um emaranhado de engrenagens enormes, presas às paredes de pedra. Algumas ainda se movem levemente, rangendo como se estivessem prestes a se soltar. O ar é pesado, impregnado de ferrugem e poeira antiga, e qualquer movimento errado pode provocar um colapso")
+desfiladeiro_estreito = N('Desfiladeiro_Estreito',
+                          "O caminho se fecha abruptamente entre duas paredes altas de pedra, formando um corredor natural. O som metálico se intensifica aqui, reverberando de forma distorcida. Cada passo ecoa, e o espaço apertado dá a sensação de que não há como recuar rapidamente.")
+margem_pedregosa = N("Margem_Pedregosa",
+                     "Ao alcançar a outra margem, o terreno se eleva em pedras irregulares. Símbolos rudimentares estão marcados em algumas rochas, como se alguém tivesse deixado avisos ou orientações. O local transmite a sensação de que já foi usado como passagem antes.")
+ilhota_submersa = N("Ilhota_Submersa", 
+                    "Uma pequena elevação de pedras emerge no meio da água. Algo reflete a luz sob a superfície, chamando atenção. Porém, o nível da água parece subir lentamente, e a corrente ao redor da ilhota fica cada vez mais forte.")
+interior_do_moinho = N("Interior_do_Moinho",
+                       "O interior é escuro e abafado. O chão está coberto de serragem antiga, madeira podre e poeira. No centro, um eixo profundo desce para a escuridão, com um alçapão parcialmente aberto, sugerindo um caminho que poucos ousariam seguir.")
+caminho_dos_campos = N("Caminho_dos_Campos",
+                       "Um caminho aberto se estende para campos abandonados. A vegetação cresce de forma desordenada, e o silêncio é tão intenso que chega a incomodar. Não há sinais recentes de passagem, apenas o vento balançando a grama seca.")
 corrente_ar = N('Corrente_de_ar',
                 'Uma corrente de ar frio sopra por uma fenda. Você sente ela lhe convidando e chega a uma entrada de caverna ao lado de uma escora rochosa com um caminho estreito que você pode passar calmamente')
 
@@ -271,7 +291,13 @@ penhasco.opcoes = {'1': final_topo,
 acampamento.opcoes = {'1': acampamento_antigo, 
                       '2': final_sabedoria}
 ponte_arriscada.opcoes = {'1': ponte_suspensa, 
-                          '2': final_queda}
+                          '2': final_queda,
+                          '3': atravessar_com_cuidado,
+                          '4': descer_pelo_abismo}
+atravessar_com_cuidado.opcoes = {'1': plataforma_oposta,
+                                 '2': final_queda}
+descer_pelo_abismo.opcoes = { '1': caverna_entrada,
+                             '2': passagem_desmoronada}
 neblina.opcoes = {'1': acampamento, 
                   '2': riacho, 
                   '3': final_fog}
@@ -290,7 +316,13 @@ acampamento_antigo.opcoes = {'1': final_tesouro,
                              '2': trilha_longe, 
                              '3': moinho}
 moinho.opcoes = {'1':  trilha_antiga,
-                 '2': final_sucesso_parcial}
+                 '2': interior_do_moinho,
+                 '3': final_sucesso_parcial,
+                 '4': caminho_dos_campos}
+interior_do_moinho.opcoes = {'1': tunel_subterraneo,
+                             '2': final_queda_eixo}
+caminho_dos_campos.opcoes = {'1': mirante,
+                             '2': bosque_claro}
 bosque_claro.opcoes = {'1': final_sucesso_parcial, 
                        '2': moinho, 
                        '3': som_metalico}
@@ -308,9 +340,22 @@ ponte_suspensa.opcoes = {'1': barranco,
 mirante.opcoes = {'1': trilha_longe, 
                   '2': neblina, 
                   '3': pegada}
-passo_raso.opcoes = {'1': final_queda}
+passo_raso.opcoes = {'1': final_queda,
+                     '2': margem_pedregosa,
+                     '3': ilhota_submersa}
+margem_pedregosa.opcoes = {'1': pedra_escrita,
+                    '2': encosta_rochosa}
+ilhota_submersa.opcoes = {'1': pegar_objeto,
+                   '2': correnteza_fatal}
+
 refugio.opcoes = {'1': mirante, 
-                  '2': acampamento}
+                  '2': acampamento,
+                  '3': trilha_dos_sinais,
+                  '4': explorar_acampamento}
+explorar_acampamento.opcoes = {'1': trilha_antiga,
+                               '2': atalho_secreto}
+trilha_dos_sinais.opcoes = {'1': encosta_rochosa,
+                            '2': emboscada}
 encosta_rochosa.opcoes = {'1': passo_raso, 
                           '2': final_sucesso_parcial}
 ponte_de_pedra.opcoes = {'1': refugio, 
@@ -330,7 +375,17 @@ pegada.opcoes = {'1': pedra_escrita,
 trilha_antiga.opcoes = {'1': encosta_rochosa}
 tempestade.opcoes = {'1': refugio, 
                      '2': final_queda}
-som_metalico.opcoes = {'1': final_queda}
+som_metalico.opcoes = {'1': final_queda,
+                       '2': estrutura_abandonada,
+                       '3':desfiladeiro_estreito}
+estrutura_abandonada.opcoes = { '1':sala_das_engrenagens,
+                        '2': trilha_escura}
+sala_das_engrenagens.opcoes = {'1': moinho,
+                     '2': colapso_final}
+desfiladeiro_estreito.opcoes = {'1': passo_raso,
+                      '2':final_queda
+}
+
 corrente_ar.opcoes = {'1': passo_raso,
                       '2':ponte_velha}
 inicio_finais = [
